b3b5a26d23b4e1f8d09afda6e8fab05b2783a3ff
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-07-25 16:38:41 -0400

    pk

--- introducao_heranca.js
+++ introducao_heranca.js
@@ -16,11 +16,28 @@ const cachorro = function(nome,tipo){
 
      this.nome = nome
      animal01.call(this, tipo)
-    
+  // this.constructor = cachorro
+      
 } 
 cachorro.prototype = new animal01()
-cachorro.prototype = cachorro()
 
 const rex = new cachorro("will","mamifero")
-console.log(rex)
-rex
+
+
+
+for(let prop in rex){
+    if(rex.hasOwnProperty(prop)){ // so mostra as propiedades da function cachorro ou qualquer que seja o nome 
+        console.log(prop)
+    }
+}
+
+
+    // verifica se cachorro é prototyco de rex
+        console.log(Object.prototype.isPrototypeOf(rex))
+
+
+
+
+console.log(rex instanceof animal01)  //verifica se rex é uma instancia de aniaml01
+
+
