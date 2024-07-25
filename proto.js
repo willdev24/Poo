3901de9862af431898be06a67f5fcc9282c3c7c8
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-07-25 09:07:40 -0400

    prototyper

--- proto.js
+++ proto.js
@@ -1,8 +1,25 @@
 
+const animal01 = function(tipo){
+         
+      if(tipo){
+    this.tipo = tipo
+}}
 
+animal01.prototype.obiterTipo = function(){ 
+    return this.tipo }
+ animal01.prototype.tipo = "vazil"
+    
+
+    const cavalo = new animal01("mamifero")
+    const egua = new animal01("reptil")
+    const zebra = new animal01()
+    console.log(cavalo.obiterTipo())
+
+    console.log(egua.tipo)
+    console.log(zebra.obiterTipo())
+
+
+   
+Array.prototype.teste="pai"
 const arr = new Array
-
-const arr2 = Array.from({length:5})
-
-console.log(arr,arr2)
 
