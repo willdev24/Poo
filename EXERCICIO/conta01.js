2eee417ee4451d4026145f743ff6b4f159da5065
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-08-02 23:38:19 -0400

    class

--- EXERCICIO/conta01.js
+++ EXERCICIO/conta01.js
@@ -1,9 +1,21 @@
 // desaafio conta bancaria
 
 
+class clinte{
+  constructor(nome, doc , data){
+  this.nome = nome
+  this.doc = doc
+  
+  }
+  }
+
 class contaBancaria{
 
     constructor( cliente, numero){
+
+      if( this.constructor === contaBancaria){
+        throw new Error("conta bancaria é uma conta abstrada")
+      }
 
       this.cliente = cliente
       this.numero = numero
@@ -28,27 +40,44 @@ class contaBancaria{
 
 
 class contapoupaca extends contaBancaria{
-constructor( nome, num){
-super(nome, num)
+constructor( nome, data){
+super(nome, data)
+this.aniversario = data
+const datas = new Date
+this.data = datas.toLocaleString("pt-BR",{})
+
+}
+
+visualiza(){     
+  console.log(`${contaBancaria.name}:\nextrato do dia ${this.data} \nnome: ${this.cliente} \naniversario:${this.aniversario} \npossui saldo de : R$ ${this.saldo}`)
 }
 
 }
 
 
 class contacorrente extends contaBancaria{
-    constructor( nome, num){
-    super(nome, num)
+    constructor( nome){
+    super(nome)
+    this.limite = 5000 
     }
     
+    visualiza(){     
+      console.log(`\n\n${contacorrente.name}:\nnome: ${this.cliente} \ncpf: ${this.numero} \npossui saldo de : R$ ${this.saldo},  \nlimite de ${this.limite} `)
+  }
+
 }    
-    
+//const teste = new contaBancaria("teste","teste02")
 
-const user01 = new contapoupaca("willy", 2)
+
+
+const willy = new clinte('willy', 70219835225)
+
+const user01 = new contapoupaca( willy," 06/02/2000")
 user01.depositar(2000)
+user01.visualiza()
 
-
-const user02 = new contacorrente("willy", 20)
+const user02 = new contacorrente(willy)
 user02.depositar(200)
 user02.sacar(50)
-
+user02.visualiza()
 
