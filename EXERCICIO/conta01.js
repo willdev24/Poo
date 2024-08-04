37c64b5ab245af8d7eb4a6008602301dd2b99302
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-08-04 08:39:41 -0400

    pk

--- EXERCICIO/conta01.js
+++ EXERCICIO/conta01.js
@@ -1,19 +1,24 @@
-// desaafio conta bancaria
+// desaafio conta bancaria //
 
 
-class clinte{
-  constructor(nome, doc , data){
-  this.nome = nome
-  this.doc = doc
-  
-  }
-  }
+//class concreta quando pode ser instanciada igual no caso a class cliente 
+//existem tamb metodos abstratos 
 
+/*
+Em outras linguagens, bastaria colocar uma palavra-chave como abstract antes do nome da classe ou do método, 
+e seria suficiente para que o programa indicasse 
+um erro ao tentar instanciar a classe abstrata ou usar o método abstrato.
+No JS nós precisamos simular esse comportamento, fazendo mais "na mão" mesmo.
+*/
+
+
+
+  //class abstrata apenas herda mas nao é possivel instanciar  
 class contaBancaria{
 
     constructor( cliente, numero){
 
-      if( this.constructor === contaBancaria){
+      if( this.constructor === contaBancaria){ //comando para tornar a class abstrata 
         throw new Error("conta bancaria é uma conta abstrada")
       }
 
@@ -33,15 +38,21 @@ class contaBancaria{
     this.saldo =  this.saldo -this.saque 
     }
 
-  visualiza(){     
-        console.log(`${this.cliente}, numero: ${this.numero} possui saldo de : R$ ${this.saldo}`)
+  visualiza(){  
+          
+        if(this.constructor === contapoupaca){
+          throw new Error("esse metodo precisa ser sobrescido");
+        }          
+      //  console.log(`${this.cliente}, numero: ${this.numero} possui saldo de : R$ ${this.saldo}`)
     }
 }
 
 
 class contapoupaca extends contaBancaria{
-constructor( nome, data){
+
+  constructor( nome, data){
 super(nome, data)
+
 this.aniversario = data
 const datas = new Date
 this.data = datas.toLocaleString("pt-BR",{})
@@ -69,15 +80,57 @@ class contacorrente extends contaBancaria{
 //const teste = new contaBancaria("teste","teste02")
 
 
+class clinte{
+  constructor(nome, doc , data){
+ if(this.constructor === clinte){
+  throw new Error("classe abstrata nao é posivel instanciar, apenas herdar");
+}
 
-const willy = new clinte('willy', 70219835225)
+  this.nome = nome
+  this.doc = doc
+  
+  }
+}
 
-const user01 = new contapoupaca( willy," 06/02/2000")
+class pessoaFisica extends clinte{
+
+  constructor(nome, doccumento){
+super(nome, doccumento)
+  }
+}
+
+class pessoaJuridica extends clinte{
+
+  constructor(nome, doccumento){
+    super(nome, doccumento)
+  }
+
+}
+
+const willy = new pessoaFisica("willy", 70219835225)
+
+const user01 = new contapoupaca( willy, " 06/02/2000")
 user01.depositar(2000)
 user01.visualiza()
 
-const user02 = new contacorrente(willy)
+const user02 = new contacorrente()
 user02.depositar(200)
 user02.sacar(50)
 user02.visualiza()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
