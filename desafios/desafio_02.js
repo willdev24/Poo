6aa23778f25a494f6134338a84ec7cb9ac89eb3e
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-08-28 16:58:47 -0400

    desafio 2

---
+++ desafios/desafio_02.js
@@ -0,0 +1,37 @@
+
+
+const rodar = (function(){
+
+   
+
+  const pessoas ={
+
+get usuarios(){
+    let cadastrados = ["5","5","5"]
+    pessoas.usuario = this
+    return cadastrados
+},
+
+get usuario(){
+    
+  return pessoas.usuarios
+
+return 
+},
+
+set usuario(_pessoa){
+
+  
+
+}
+
+  }
+
+return pessoas
+
+})()
+
+rodar.usuario
+
+rodar.usuario
+
