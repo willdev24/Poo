daead669db8298073e8200489cc1881456b2d357
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-09-16 20:44:16 -0400

    treino

--- desafios/desafio_02.js
+++ desafios/desafio_02.js
@@ -1,2 +1,37 @@
 
 
+const testee = (function (){
+
+    let _pessoas = []
+
+  this.pessoa = {
+
+    get pessoas(){
+       return _pessoas
+
+    },
+
+    set pessoas(_valor){
+
+    const as =  _pessoas.indexOf(_valor )
+        console.log(as)
+        if( as < 0){
+             _pessoas.push(
+                _valor
+             )
+        }else{
+            throw new Error("pessoa ja cadastrada");
+            
+        }
+
+    }}
+
+return this.pessoa
+})()
+
+testee.pessoas ="willy"
+testee.pessoas ="gabriela"
+//testee.pessoas ="willy"
+
+console.log(testee.pessoas)
+
